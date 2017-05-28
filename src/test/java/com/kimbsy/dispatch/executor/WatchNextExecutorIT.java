461bd1e20b3e0b64ebb776ba0539c39a692c64fe
Kimbsy <lordkimber@gmail.com>
2017-05-28 16:27:57 +0100

    added simple integration test for WATCH_NEXT

---
+++ src/test/java/com/kimbsy/dispatch/executor/WatchNextExecutorIT.java
@@ -0,0 +1,27 @@
+package com.kimbsy.dispatch.executor;
+
+import com.kimbsy.dispatch.request.Request;
+import com.kimbsy.dispatch.request.RequestType;
+import org.junit.Test;
+
+import java.util.Collections;
+
+/**
+ * @author kimbsy
+ */
+public class WatchNextExecutorIT {
+
+    @Test
+    public void testWatchNextStarTrek() {
+        WatchNextExecutor watchNextExecutor = new WatchNextExecutor();
+        watchNextExecutor.setRequest(new Request(RequestType.WATCH_NEXT, Collections.singletonList("STAR_TREK")));
+        watchNextExecutor.execute();
+    }
+
+    @Test
+    public void testWatchNextArcher() {
+        WatchNextExecutor watchNextExecutor = new WatchNextExecutor();
+        watchNextExecutor.setRequest(new Request(RequestType.WATCH_NEXT, Collections.singletonList("ARCHER")));
+        watchNextExecutor.execute();
+    }
+}
