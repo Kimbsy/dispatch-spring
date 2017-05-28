630dfa8b22fee967c1755bcf03ce65c98f2481b4
Kimbsy <lordkimber@gmail.com>
2017-05-28 16:27:29 +0100

    better implementation of WATCH_NEXT

--- src/main/java/com/kimbsy/dispatch/executor/WatchNextExecutor.java
+++ src/main/java/com/kimbsy/dispatch/executor/WatchNextExecutor.java
@@ -1,23 +1,73 @@
 package com.kimbsy.dispatch.executor;
 
-import java.io.*;
+import com.google.common.base.Preconditions;
+
+import java.io.IOException;
 
 /**
  * This class is responsible for invoking the <code>watchnext</code> tool on <code>apotheosis</code>.
+ *
  * @author kimbsy
  */
 public class WatchNextExecutor extends AbstractExecutor<Boolean> {
+
+    /**
+     * The available series with the name of their containing directory.
+     */
+    public enum Series {
+        STAR_TREK("star-trek"),
+        ARCHER("archer"),
+        RICK_AND_MORTY("rick-and-morty"),
+        SILICON_VALLEY("silicon-valley"),;
+
+        private String dirName;
+
+        Series(String dirName) {
+            this.dirName = dirName;
+        }
+
+        public String getDirName() {
+            return dirName;
+        }
+    }
 
     /**
      * @inheritDoc
      */
     @Override
     public Boolean execute() {
-        log.info("Executing WATCH_NEXT");
+        Preconditions.checkNotNull(request, "request cannot be null");
+
+        Series series = Series.valueOf(request.getArguments().get(0));
+        Preconditions.checkNotNull(series, "request series cannot be null");
+
         try {
-            // @TODO: should specify what to watch?
-            // @TODO: can we use a command in /usr/bin/lib?
-            Runtime.getRuntime().exec("ssh apotheosis './Tools/test/watchnext'");
+            log.info("Executing WATCH_NEXT");
+            ProcessBuilder pb = new ProcessBuilder("ssh",
+                    "apotheosis",
+                    "watchnext " + series.getDirName(),
+                    "&");
+            Process p = pb.start();
+
+//            BufferedReader stdOut = new BufferedReader(new
+//                    InputStreamReader(p.getInputStream()));
+//
+//            BufferedReader stdError = new BufferedReader(new
+//                    InputStreamReader(p.getErrorStream()));
+//
+//            // read the output from the command
+//            String s;
+//            System.out.println("Here is the standard output of the command:\n");
+//            while ((s = stdOut.readLine()) != null) {
+//                System.out.println(s);
+//            }
+//
+//            // read any errors from the attempted command
+//            System.out.println("Here is the standard error of the command (if any):\n");
+//            while ((s = stdError.readLine()) != null) {
+//                System.out.println(s);
+//            }
+
             System.exit(0);
             return true;
         } catch (IOException e) {
